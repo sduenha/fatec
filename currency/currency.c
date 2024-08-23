917466c3743de3ad5da54f3e80f39ee2a91914d2
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-08-23 14:01:27 +0000

    Aug 23, 2024, 2:01 PM

--- currency/currency.c
+++ currency/currency.c
@@ -9,14 +9,16 @@ int main(void) {
     float iene = 26.32;
     float renminbi = 1.28;
 
-    printf("1 - Dolar\n 2 - Euro\n 3 - Iene\n 4 - Renminbi\n");
+    printf("1 - Dolar\n2 - Euro\n3 - Iene\n4 - Renminbi\n");
     printf("Escolha moeda de entrada: ");
     scanf("%i", &moeda_entrada);
 
     printf("Digite o valor para converter: ");
     scanf("%f", &valor);
 
-    printf("1 - Dolar\n 2 - Euro\n 3 - Iene\n 4 - Renminbi\n");
+    printf("1 - Dolar\n2 - Euro\n3 - Iene\n4 - Renminbi\n");
     printf("Escolha moeda de saída: ");
     scanf("%i", &moeda_saida);
+
+    if ()
 }

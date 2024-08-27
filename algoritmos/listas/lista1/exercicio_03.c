5c59f5abc7fea86e4daa95034930eabf914c744d
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-08-27 14:34:10 +0000

    Aug 27, 2024, 2:34 PM

--- algoritmos/listas/lista1/exercicio_03.c
+++ algoritmos/listas/lista1/exercicio_03.c
@@ -3,7 +3,10 @@
 
 int main(void) {
     int x;
+    float resultado;
 
     printf("Insira um valor para x: ");
     scanf("%i", &x);
+
+    printf("O valor de f(x) = sqrt(x) + (x/2) + x^x para x = %i é: %f\n", x, resultado)
 }

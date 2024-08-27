1d55f1371532829e81e0156ad5d00c2379f3fe79
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-08-27 13:28:39 +0000

    Aug 27, 2024, 1:28 PM

--- change/change.c
+++ change/change.c
@@ -3,5 +3,19 @@
 int main(void) {
     int v1, v2, v3;
 
-    
+    printf("Valor 1: ");
+    scanf("%i", &v1);
+
+    printf("Valor 2: ");
+    scanf("%i", &v2);
+
+    printf("v1 = %i\n", v1);
+    printf("v2 = %i\n", v2);
+
+    v3 = v2;
+    v2 = v1;
+    v1 = v2;
+
+    printf("v1 = %i\n", v1);
+    printf("v2 = %i\n", v2);
 }

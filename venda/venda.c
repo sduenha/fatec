b84723f42f2b3b5d7b4442b99966290ae793d0ca
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-08-27 12:46:39 +0000

    Aug 27, 2024, 12:46 PM

--- venda/venda.c
+++ venda/venda.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+    int vendedor, percentual;
     float venda, comissão;
+
+    printf("Número do vendedor: ");
+    scanf("%i", &vendedor);
 
     printf("Valor da venda: ");
     scanf("%f", &venda);
 
+    printf("Porcentagem da comissão: ");
+    scanf("%i", &percentual);
+
     
+
 }

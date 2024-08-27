5839fd29a6ff5d5578f4a12fc51336db4eed8bd7
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-08-27 14:42:59 +0000

    Aug 27, 2024, 2:42 PM

---
+++ algoritmos/listas/lista1/exercicio_04.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+
+    printf("Escolha o valor de a: ");
+    scanf("%i", &a);
+
+    printf("Escolha o valor de b: ");
+    scanf("%i", &b);
+}

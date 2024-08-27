18d509a7dc645574f9ffd9abab5f1f759d56aa12
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-08-27 13:41:01 +0000

    Aug 27, 2024, 1:41 PM

--- change/change.c
+++ change/change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    int v1, v2, v3;
+    int v1, v2, aux;
 
     printf("Valor 1: ");
     scanf("%i", &v1);
@@ -13,9 +13,9 @@ int main(void) {
     printf("v1 = %i\n", v1);
     printf("v2 = %i\n\n", v2);
 
-    v3 = v2;
+    aux = v2;
     v2 = v1;
-    v1 = v3;
+    v1 = aux;
 
     printf("Depois:\n");
     printf("v1 = %i\n", v1);

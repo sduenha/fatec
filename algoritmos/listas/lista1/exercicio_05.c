f5b3e79cc8a80f1c78389f4c04ce92bd6ef3bfb6
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-08-30 12:52:19 +0000

    Aug 30, 2024, 12:52 PM

---
+++ algoritmos/listas/lista1/exercicio_05.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+
+int main(void) {
+    int a, b, c;
+
+    printf("Insira os valores de um triângulo:\n");
+    printf("A: ");
+    scanf("%i", &a);
+
+    printf("B: ");
+    scanf("%i", &b);
+
+    printf("C: ");
+    scanf("%i", &c);
+
+    if(a == b && b == c) {
+        printf("O Triângulo é isósceles");
+    }else if ()
+}

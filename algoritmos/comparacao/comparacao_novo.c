eb1506459832f4dcc491f1c7276255c687ec0f83
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-03 12:53:21 +0000

    Sep 3, 2024, 12:53 PM

---
+++ algoritmos/comparacao/comparacao_novo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int num;
+
+    printf("Digite um valor: ");
+    scanf("%i", &num);
+
+
+    if (num >= 100) {
+        if (num % 2 == 0){
+            printf("Par e maior ou igual a 100\n");
+        } else {
+            printf("Ímpar maior ou igual a 100\n");
+        }
+    } else {
+        if (num % 2 == 0) {
+            printf("Par menor que 100\n");
+        } else {
+            printf("Ímpar menos que 100\n");
+        }
+    }
+}

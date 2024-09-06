0a631a1d897998d41e9c6e354ba470dcc82af523
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-06 13:27:37 +0000

    Sep 6, 2024, 1:27 PM

--- algoritmos/ordem/ordem.c
+++ algoritmos/ordem/ordem.c
@@ -6,7 +6,13 @@ int main(void) {
     printf("Digite 3 valores: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a > b && b < c && a > c) {
-        printf("%i %i %i", a, b, c);
+    if (a > b && a > c) {
+        if (b > c) {
+            printf("%i %i %i", a, b, c);
+            return 0;
+        } else {
+            printf("%i %i %i", a, c, b);
+            return 0;
+        }
     }
 }

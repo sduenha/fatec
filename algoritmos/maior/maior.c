44e6f86be94f8eea3d45803f8776e20bf812906a
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-06 13:09:26 +0000

    Sep 6, 2024, 1:09 PM

--- algoritmos/maior/maior.c
+++ algoritmos/maior/maior.c
@@ -3,5 +3,25 @@
 int main(void) {
     int a, b, c, maior;
 
+    printf("Digite o primeiro valor: ");
+    scanf("%i", &a);
+
+    printf("Digite o segundo valor: ");
+    scanf("%i", &b);
+
+    if (a > b) {
+        maior = a;
+    } else {
+        maior = b;
+    }
+
+    printf("Digite o terceiro valor: ");
+    scanf("%i", &c);
+
+    if (c > maior) {
+        maior = c;
+    }
+
+    printf("O maior valor é o %i\n", maior);
     
 }

17fc02a6064be4089d24744c00ccdd1e6e2d1f76
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-06 14:09:59 +0000

    Sep 6, 2024, 2:09 PM

--- algoritmos/area/area.c
+++ algoritmos/area/area.c
@@ -4,24 +4,22 @@ int main(void) {
     char tipo;
     float lado1, lado2, raio;
     printf("Escolha o objeto para calcular a área: \nq: Quadrado\nr: Retângulo\nc: Círculo\nEscolha: ");
-    scanf("%i", &tipo);
-
-    printf("%i\n", tipo);
+    scanf("%c", &tipo);
 
     if (tipo == 113){
         printf("Insira as dimensões (tamanho do lado): ");
         scanf("%f", &lado1);
 
-        printf("A área desse quadrado é %f\n", lado1 * lado1);
-    } else if(tipo == "r") {
+        printf("A área desse quadrado é %.2f\n", lado1 * lado1);
+    } else if(tipo == 114) {
         printf("Insira as dimensões (tamanhos dos dois lados): ");
         scanf("%f %f", &lado1, &lado2);
 
-        printf("A área desse retângulo é %f\n", lado1 * lado2);
-    }else if(tipo == "c") {
+        printf("A área desse retângulo é %.2f\n", lado1 * lado2);
+    }else if(tipo == 99) {
         printf("Insira o raio: ");
         scanf("%f", &raio);
-        printf("A área desse círculo é %f\n", (3,14) * raio * raio);
+        printf("A área desse círculo é %.2f\n", (3,14) * raio * raio);
     } else {
         printf("Valor inválido!\n");
         return 1;

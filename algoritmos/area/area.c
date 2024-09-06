d3cce524244db8050755c1d77d9299cdfa133d20
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-06 14:02:20 +0000

    Sep 6, 2024, 2:02 PM

--- algoritmos/area/area.c
+++ algoritmos/area/area.c
@@ -9,12 +9,17 @@ int main(void) {
     if (tipo == "q"){
         printf("Insira as dimensões (tamanho do lado): ");
         scanf("%f", &lado1);
+
+        printf("A área desse quadrado é %f\n", lado1 * lado1);
     } else if(tipo == "r") {
         printf("Insira as dimensões (tamanhos dos dois lados): ");
         scanf("%f %f", &lado1, &lado2);
+
+        printf("A área desse retângulo é %f\n", lado1 * lado2);
     }else if(tipo == "c") {
         printf("Insira o raio: ");
         scanf("%f", &raio);
+        printf("A área desse círculo é %f\n", (3,14) * raio * raio);
     } else {
         printf("Valor inválido!\n");
         return 1;

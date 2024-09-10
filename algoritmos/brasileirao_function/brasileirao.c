4e0f24f0993753f5e1c0b4524d06ec9d1d1dcc6d
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-10 13:57:59 +0000

    Sep 10, 2024, 1:57 PM

--- algoritmos/brasileirao_function/brasileirao.c
+++ algoritmos/brasileirao_function/brasileirao.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-char comparacao(int a, int b, int ordem);
+// char comparacao(int a, int b, int ordem);
 
 int main(void) {
     int valores_a[5], valores_b[5];
+    char parametros[5][20] = {"vitorias", "saldo de gols", "gols", "cartões Vermelhos", "cartões Amarelos"};
+    char vencedor;
 
     for (int i = 0; i < 5; i++) {
-
+        printf("Digite o valor de %s: ", parametros[i]);
+        scanf("%i %i", &valores_a[i], &valores_b[i]);
+        vencedor = comparacao(valores_a[i], valores_b[i], 0);
+        if (vencedor == "a" || vencedor == "b") {
+            printf("O vencedor foi o time %c", vencedor);
+            return 0;
+        }
     }
-
-    printf("Vitórias do time A: ");
-    scanf("%i", &valores_a[0]);
-
-    printf("Vitórias do time B: ");
-    scanf("%i", &valores_b[0]);
 }
 
 char comparacao(int a, int b, int ordem) {

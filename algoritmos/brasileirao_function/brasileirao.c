df3a97d40470a9dd067d337f90102788a0072489
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-10 18:05:10 +0000

    Sep 10, 2024, 6:05 PM

--- algoritmos/brasileirao_function/brasileirao.c
+++ algoritmos/brasileirao_function/brasileirao.c
@@ -6,18 +6,23 @@ char vencedor;
 int main(void) {
     int valores_a[5], valores_b[5];
     char parametros[5][20] = {"vitorias", "saldo de gols", "gols", "cartões Vermelhos", "cartões Amarelos"};
-    char vencedor;
+    int vencedor_num;
 
     for (int i = 0; i < 5; i++) {
         printf("Digite o valor de %s: ", parametros[i]);
         scanf("%i %i", &valores_a[i], &valores_b[i]);
         if (i == 3 || i == 4) {
-            vencedor = comparacao(valores_a[i], valores_b[i], 1);
+            vencedor_num = comparacao(valores_a[i], valores_b[i], 1);
         } else {
-            vencedor = comparacao(valores_a[i], valores_b[i], 0);
+            vencedor_num = comparacao(valores_a[i], valores_b[i], 0);
         }
-        if (vencedor == "a" || vencedor == "b") {
-            printf("O vencedor foi o time %c", vencedor);
+        if (vencedor_num == 1 || vencedor_num == 2) {
+            printf("O vencedor foi o time %c\n", vencedor);
+            return 0;
+        }
+
+        if (i == 5 && vencedor_num == 0) {
+            printf("Os times continuam empatados!\n");
             return 0;
         }
     }
@@ -27,16 +32,20 @@ int comparacao(int a, int b, int ordem) {
     if (ordem) {
         if(a > b) {
             vencedor = a;
+            return 1;
         } else if (b > a) {
-            vencedor b;
+            vencedor = b;
+            return 2;
         } else {
             return 0;
         }
     } else {
         if (a > b) {
-            return b;
+            vencedor = b;
+            return 2;
         } else if (b > a){
-            return a;
+            vencedor = a;
+            return 1;
         } else {
             return 0;
         }

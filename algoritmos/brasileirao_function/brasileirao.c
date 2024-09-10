e6f273ae1f920291a785592cd7aeb79f71814614
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-10 13:34:00 +0000

    Sep 10, 2024, 1:34 PM

--- algoritmos/brasileirao_function/brasileirao.c
+++ algoritmos/brasileirao_function/brasileirao.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-char comparacao(int a, int b);
+// char comparacao(int a, int b);
 
 int main(void) {
-    int valor_a, valor_b;
+    int valores_a[5], valores_b[5];
+
+    for (int i = 0; i < 5; i++) {
+        
+    }
 
     printf("Vitórias do time A: ");
-    scanf("%i", &valor_a);
+    scanf("%i", &valores_a[0]);
 
     printf("Vitórias do time B: ");
-    scanf("%i", &valor_b);
+    scanf("%i", &valores_b[0]);
 
-    comparacao(valor_a, valor_b);
-}
 
-char comparacao(int a, int b) {
-    
+    // comparacao(valor_a, valor_b);
 }

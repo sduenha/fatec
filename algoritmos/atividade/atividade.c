75baf55979f0a00e728071e338c564fcd25425c6
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-17 20:03:40 +0000

    Sep 17, 2024, 8:03 PM

--- algoritmos/atividade/atividade.c
+++ algoritmos/atividade/atividade.c
@@ -62,7 +62,7 @@ int main(void) {
         }
     } else if (mes2 == 2){
         // Validando se é ano bissexto para a quantida de dias em Fevereiro
-        if ((ano1 % 4 == 0 && ano1 % 100 != 0) || (ano1 % 400 == 0)) {
+        if ((ano2 % 4 == 0 && ano2 % 100 != 0) || (ano2 % 400 == 0)) {
             if (dia2 > 29) {
                 printf("O dia da segunda data é inválido\n");
                 return 0;

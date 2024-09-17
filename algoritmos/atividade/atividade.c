9f541c00283f09afc25aa7004bfbf0917c8af1a3
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-17 19:25:50 +0000

    Sep 17, 2024, 7:25 PM

--- algoritmos/atividade/atividade.c
+++ algoritmos/atividade/atividade.c
@@ -7,10 +7,6 @@ int main(void) {
     printf("Como o exemplo: 19 8 2024\n");
     printf("Sua data: ");
     scanf("%d %d %d", &dia1, &mes1, &ano1);
-
-    printf("Insira outra data (somente os números): ");
-    scanf("%d %d %d", &dia2, &mes2, &ano2);
-
     if (mes1 == 4 || mes1 == 6 || mes1 == 9 || mes1 == 11) {
         if (dia1 > 30) {
             printf("O dia da primeira data é inválido\n");
@@ -20,30 +16,57 @@ int main(void) {
         if (ano1 % 4 == 0 && ano1 % 100 == 1 || ano1 % 400 == 0) {
             if (dia1 > 29) {
                 printf("O dia da primeira data é inválido\n");
+                return 0;
             }
         } else {
             if (dia1 > 28) {
                 printf("O dia da primeira data é inválido\n");
+                return 0;
             }
         }
     } else {
         if (dia1 > 31) {
             printf("O dia da primeira data é inválido\n");
+            return 0;
         }
     }
 
-    2 4 6 9 11
-
-    if (dia1 > 31 || dia1 < 1 || dia2 > 31 || dia2 < 1) {
-        printf("Os dias forncecidos são inválidos!\n");
-        return 0;
-    } else if (mes1 > 12 || mes1 < 1 || mes2 > 12 || mes2 < 1) {
-        printf("Os meses forncecidos são inválidos!\n");
-        return 0;
-    } else if (ano1 < 0 || dia2 < 0) {
-        printf("Os anos fornecidos são inválidos!\n");
-        return 0;
+    printf("Insira outra data (somente os números): ");
+    scanf("%d %d %d", &dia2, &mes2, &ano2);
+    if (mes2 == 4 || mes2 == 6 || mes2 == 9 || mes2 == 11) {
+        if (dia2 > 30) {
+            printf("O dia da segunda data é inválido\n");
+            return 0;
+        }
+    } else if (mes2 == 2){
+        if (ano2 % 4 == 0 && ano2 % 100 == 1 || ano2 % 400 == 0) {
+            if (dia2 > 29) {
+                printf("O dia da segunda data é inválido\n");
+                return 0;
+            }
+        } else {
+            if (dia2 > 28) {
+                printf("O dia da segunda data é inválido\n");
+                return 0;
+            }
+        }
+    } else {
+        if (dia2 > 31) {
+            printf("O dia da segunda data é inválido\n");
+            return 0;
+        }
     }
+
+    // if (dia1 > 31 || dia1 < 1 || dia2 > 31 || dia2 < 1) {
+    //     printf("Os dias forncecidos são inválidos!\n");
+    //     return 0;
+    // } else if (mes1 > 12 || mes1 < 1 || mes2 > 12 || mes2 < 1) {
+    //     printf("Os meses forncecidos são inválidos!\n");
+    //     return 0;
+    // } else if (ano1 < 0 || dia2 < 0) {
+    //     printf("Os anos fornecidos são inválidos!\n");
+    //     return 0;
+    // }
 
     if (ano1 == ano2) {
         if (mes1 == mes2) {

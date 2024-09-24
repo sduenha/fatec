71e87ad1dbba9f36588df4292a7401bd50be53dd
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-24 15:35:27 +0000

    Sep 24, 2024, 3:35 PM

--- algoritmos/desafio_01/desafio1.c
+++ algoritmos/desafio_01/desafio1.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
+    // Declarando as variáveis
     char v1, v2, v3, v4, v5, v6, v7, v8, v9;
     char test = 'X';
 
+    // Pedindo ao usuário os valores do jogo
     printf("Digite o jogo: \n");
     scanf("%c %c %c %c %c %c %c %c %c", &v1, &v2, &v3, &v4, &v5, &v6, &v7, &v8, &v9);
 
+    // Iniciando um for para verificar as possíbilidades
     for (int i = 0; i < 2; i ++) {
+        // Verificando todas as possibilidades de vitória
         if ((v1 == test && v2 == test && v3 == test) || (v4 == test && v5 == test && v6 == test) || (v7 == test && v8 == test && v9 == test) || (v1 == test && v4 == test && v7 == test) || (v2 == test && v5 == test && v8 == test) || (v3 == test && v6 == test && v9 == test) || (v1 == test && v5 == test && v9 == test) || (v3 == test && v5 == test && v7 == test) ) {
             printf("%c ganhou!\n", test);
             return 0;
         }
+        // Trocando o verificador de vitória
         test = 'O';
     }
-    printf("Empate!");
+    // Printando empate caso em cima não dê certo
+    printf("Empatou!\n");
 }

1b754141944768075650dd246c149e38c904a53a
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-09-24 15:15:00 +0000

    Sep 24, 2024, 3:15 PM

--- algoritmos/desafio_01/desafio1.c
+++ algoritmos/desafio_01/desafio1.c
@@ -5,8 +5,10 @@ int main(void) {
 
     printf("Digite o jogo: \n");
     scanf("%c %c %c %c %c %c %c %c %c", &v1, &v2, &v3, &v4, &v5, &v6, &v7, &v8, &v9);
-    
-    if (v1 == v2 == v3 == 'x') {
+
+    printf("%c %c %c\n", v1, v2, v3);
+
+    if (v1 == v2 == v3 == 'X') {
         printf("x ganhou!");
     }
 }

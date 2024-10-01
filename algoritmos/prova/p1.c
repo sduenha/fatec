b674689bf7f8607fa644909dd97f1d7439388408
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-01 13:19:04 +0000

    Oct 1, 2024, 1:19 PM

--- algoritmos/prova/p1.c
+++ algoritmos/prova/p1.c
@@ -7,13 +7,20 @@ int main(void) {
     scanf("%i", &eleitor1);
     if (eleitor1 == 1) {
         candidato1 ++;
-        sequencia = 1;
     } else if (eleitor1 == 2) {
         candidato2 ++;
-        sequencia = 2;
     } else if (eleitor1 == 3) {
+        candidato3 ++;
+    }
+
+    if (eleitor2 == 1) {
+        candidato1 ++;
+        sequencia = 1;
+    } else if (eleitor2 == 2) {
+        candidato2 ++;
+        sequencia = 2;
+    } else if (eleitor2 == 3) {
         candidato3 ++;
         sequencia = 3;
     }
-
 }

4521538ca6b270d988a0396acced24c173690ba9
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-01 13:17:28 +0000

    Oct 1, 2024, 1:17 PM

--- algoritmos/prova/p1.c
+++ algoritmos/prova/p1.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+int main(void) {
+    int candidato1, candidato2, candidato3, eleitor1, eleitor2, eleitor3, eleitor4, sequencia;
+
+    printf("Eleitor 1, selecione o seu candidato: ");
+    scanf("%i", &eleitor1);
+    if (eleitor1 == 1) {
+        candidato1 ++;
+        sequencia = 1;
+    } else if (eleitor1 == 2) {
+        candidato2 ++;
+        sequencia = 2;
+    } else if (eleitor1 == 3) {
+        candidato3 ++;
+        sequencia = 3;
+    }
+
+}

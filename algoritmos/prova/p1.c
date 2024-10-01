d965222df697133775490ea9f443d0ef50240089
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-01 13:46:04 +0000

    Oct 1, 2024, 1:46 PM

--- algoritmos/prova/p1.c
+++ algoritmos/prova/p1.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int candidato1, candidato2, candidato3, eleitor1, eleitor2, eleitor3, eleitor4, sequencia1, sequencia2, sequencia3;
+    int candidato1 = 0;
+    int candidato2 = 0;
+    int candidato3 = 0;
+    int eleitor1 = 0; 
+    int eleitor2 = 0;
+    int eleitor3 = 0;
+    int eleitor4 = 0;
+    int sequencia1 = 0;
+    int sequencia2 = 0;
+    int sequencia3 = 0;
 
     printf("Eleitor 1, selecione o seu candidato: ");
     scanf("%i", &eleitor1);
@@ -35,20 +44,62 @@ int main(void) {
     printf("Eleitor 3, selecione o seu candidato: ");
     scanf("%i", &eleitor3);
     if (eleitor3 == 1) {
+        if (eleitor3 == eleitor2) {
+            sequencia1 ++;
+        }
         candidato1 ++;
     } else if (eleitor3 == 2) {
+        if (eleitor3 == eleitor2) {
+            sequencia2 ++;
+        }
         candidato2 ++;
     } else if (eleitor3 == 3) {
+        if (eleitor3 == eleitor2) {
+            sequencia3 ++;
+        }
         candidato3 ++;
     }
 
     printf("Eleitor 4, selecione o seu candidato: ");
     scanf("%i", &eleitor4);
     if (eleitor4 == 1) {
+        if (eleitor4 == eleitor3) {
+            sequencia1 ++;
+        }
         candidato1 ++;
     } else if (eleitor4 == 2) {
+        if (eleitor4 == eleitor3) {
+            sequencia2 ++;
+        }
         candidato2 ++;
     } else if (eleitor4 == 3) {
+        if (eleitor4 == eleitor3) {
+            sequencia3 ++;
+        }
         candidato3 ++;
+    }
+
+    if (candidato1 == candidato2) {
+        if (sequencia1 > sequencia2) {
+            printf("O candidato 1 venceu!\n");
+        } else if (sequencia1 < sequencia2) {
+            printf("O candidato 2 venceu!\n");
+        }
+    } else if (candidato1 == candidato3) {
+        if (sequencia1 > sequencia3) {
+            printf("O candidato 1 venceu!\n");
+        } else if (sequencia1 < sequencia3) {
+            printf("O candidato 3 venceu!\n");
+        }
+    } else if (candidato2 == candidato3) {
+        if (sequencia2 > sequencia3) {
+            printf("O candidato 2 venceu!\n");
+        } else if (sequencia2 < sequencia3) {
+            printf("O candidato 3 venceu!\n");
+        }
+    } else {
+        printf("%i\n", sequencia1);
+        printf("%i\n", sequencia3);
+        printf("Empate\n");
     }
 }

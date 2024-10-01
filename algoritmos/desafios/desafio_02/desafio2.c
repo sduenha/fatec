155ec4243d0e40e90af1d18e1403db8dcaa1beb2
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-01 18:03:45 +0000

    Oct 1, 2024, 6:03 PM

--- algoritmos/desafios/desafio_02/desafio2.c
+++ algoritmos/desafios/desafio_02/desafio2.c
@@ -10,24 +10,20 @@ int main(void) {
     printf("Valendo!\n");
     int round = 0;
     int fim = 0;
-    int ataque = 0;
+    int validador = 0;
     int ken, ryu;
     int golpe;
 
     while (round != 1) {
         scanf("%i", &golpe);
-        if (golpe > 0) {
-            ataque = 1;
-        }
-        if (ataque = 0) {
+        if (golpe > 0 && validador == 0) {
             ryu += testNum(golpe);
-        } else {
-            ataque = 1;
-        }
-        if (golpe < 0) {
-            golpe *= -1;
+        } else if (golpe < 0) {
             ken += testNum(golpe);
-        } else {
+            validador == 1;
+        } else if (golpe > 0 && validador == 1) {
+            round = 1;
+            printf("Fim do primeiro round!\n");
         }
     }
 }

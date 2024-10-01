3e1af61688cdea1fbee06d749e4823c1600e3051
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-01 17:41:28 +0000

    Oct 1, 2024, 5:41 PM

--- algoritmos/desafios/desafio_02/desafio2.c
+++ algoritmos/desafios/desafio_02/desafio2.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+int testNum(int num);
+int testPerfeito(int num);
+int testTriangular(int num);
+
 int main(void) {
-    
+    int troca = 0;
+
+    while (troca != 1) {
+
+    }
+}
+
+int testNum(int num) {
+    novoNum = testPerfeito(num);
+    if (testPerfeito)
+}
+
+int testPerfeito(int num) {
+
+}
+
+int testTriangular(int num) {
+
 }

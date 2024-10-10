6ad27569dd3b75a4368b0e45cc221bacb6ae271c
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-10 19:31:22 +0000

    Oct 10, 2024, 7:31 PM

--- algoritmos/desafios/desafio_02/desafio2.c
+++ algoritmos/desafios/desafio_02/desafio2.c
@@ -12,12 +12,16 @@ int testTriangular(int num);
 int resultado(int ryu, int ken);
 
 int main(void) {
+    // Explicando para o usuário como o jogo funciona
     printf("Início da luta!\nValores positivos são os ataques do Ryu\nValores negativos são os ataques sofridos pelo Ryu\n");
     printf("Insira o valor dos golpes!\n");
     printf("Valendo!\n");
+
+    // Iniciando variáveis do código
     int round = 0, validador = 0, golpe = 1, ultimo = 0;
     int ryu[2] = {0,0}, ken[2] = {0,0};
     
+    // Iniciando o loop que vai durar até o usuário finalizar o jogo, digitando "0"
     while (golpe != 0) {
         if (ultimo != 0) {
             ryu[1] += testNum(ultimo);

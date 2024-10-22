0c715932ba09196c35e2a5e147ae295fa14eed8c
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-22 12:58:58 +0000

    Oct 22, 2024, 12:58 PM

---
+++ algoritmos/fibonacci/fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, soma, n1, n2;
+
+    printf("Número: ");
+    scanf("%i", &n);
+    printf("%i\n", n);
+
+    for (int i = 1; i < n; i++) {
+        
+    }
+}

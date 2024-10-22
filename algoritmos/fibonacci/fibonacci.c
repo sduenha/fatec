df0a0dff3000e1764c61e9d2fcec2847dc7d8131
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-10-22 13:09:14 +0000

    Oct 22, 2024, 1:09 PM

--- algoritmos/fibonacci/fibonacci.c
+++ algoritmos/fibonacci/fibonacci.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-    int n, soma = 0, atual = 1, anterior = 1;
+    int n, soma = 0, atual = 1, anterior = 0;
 
     printf("Número: ");
     scanf("%i", &n);
 
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         soma = atual + anterior;
         anterior = atual;
         atual = soma;
     }
-    printf("%i\n", soma);
+
+    if (soma == 0) {
+        printf("1");
+    } else {
+        printf("%i\n", soma);
+    }
 }

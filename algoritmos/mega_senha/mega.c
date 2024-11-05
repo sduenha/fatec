0badd2251367611dc69585d5d12a74fb69f099ac
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-05 13:31:24 +0000

    Nov 5, 2024, 1:31 PM

--- algoritmos/mega_senha/mega.c
+++ algoritmos/mega_senha/mega.c
@@ -2,7 +2,12 @@
 
 int main(void) {
     int d1, d2, d3, d4, d5, d6;
-    for (int i = 0; i < 6; i++) {
-        
-    }
+
+    for(d1 = 1; d1 <= 55; d1++)
+        for(d2 = 1; d2 <= 56; d2++)
+            for(d3 = 1; d3 <= 57; d3++)
+                for(d4 = 1; d4 <= 58; d4++)
+                    for(d5 = 1; d5 <= 59; d5++)
+                        for(d6 = 1; d6 <= 60; d6++)
+                            printf("%d %d %d %d %d %d\n", d1, d2, d3, d4, d5, d6);
 }

366a19ff4da6a4f14073e96b452e89db219155c3
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-05 13:04:36 +0000

    Nov 5, 2024, 1:04 PM

--- algoritmos/mega_senha/mega.c
+++ algoritmos/mega_senha/mega.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-    int num = 0;
+    int d1, d2, d3, d4, d5, d6;
     for (int i = 0; i < 6; i++) {
-        num = rand();
-        printf(num);
+        
     }
 }

1b2c31973b8f11f8f284bcddadc405a51998e523
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-06 14:31:17 +0000

    Nov 6, 2024, 2:31 PM

--- algoritmos/listas/lista2/exercicio_02.c
+++ algoritmos/listas/lista2/exercicio_02.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int a, b;
+    int a, b, value = 1;
 
-    print("Escolha dois números, separados com espaço: ");
+    printf("Escolha dois números, separados com espaço: ");
     scanf("%d %d", &a, &b);
 
     for (int i = 1; i <= b; i++) {
-        a *= b;
+        value *= a;
     }
+
+    printf("Valor: %d\n", value);
 }

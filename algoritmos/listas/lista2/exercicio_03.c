7c96cbf16fa70197bf6ed83c0c737fae9e1de562
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-06 14:43:42 +0000

    Nov 6, 2024, 2:43 PM

--- algoritmos/listas/lista2/exercicio_03.c
+++ algoritmos/listas/lista2/exercicio_03.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int n, valor = 1;
+    int n, valor = 0;
 
     printf("Escolha um número: ");
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++){
-        valor ++;
+        valor += i;
     }
 
     printf("Valor: %d\n", valor);

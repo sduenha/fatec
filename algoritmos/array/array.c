ede7f5cc0a5ab1c498406bfa427b4c21cd84bf99
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-08 13:30:57 +0000

    Nov 8, 2024, 1:30 PM

--- algoritmos/array/array.c
+++ algoritmos/array/array.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    int vetor1[5] = {1, 2, 3, 4, 5};
-    int vetor2[5] = {1, 2, 3, 4, 5};
-    int vetor3[5];
+    int vetor1[5], vetor2[5], vetor3[5];
 
     for (int i = 0; i < 5; i++) {
+        printf("Digite dois valores: ");
+        scanf("%d %d", &vetor1[i], &vetor2[i]);
         vetor3[i] = vetor1[i] * vetor2[i];
         printf("%d ", vetor3[i]);
     }

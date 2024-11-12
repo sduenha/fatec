783e714ee1469b69b2b4c57e75bc0d8f5a473572
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-12 13:12:29 +0000

    Nov 12, 2024, 1:12 PM

--- algoritmos/read_array/read.c
+++ algoritmos/read_array/read.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-    int 
+    int vetor1[5] = {0, 1, 2, 3, 4};
+    int vetor2[5] = {5, 6, 7, 8, 9};
+    int iguais = 0;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (vetor1[i] == vetor2[j]) {
+                iguais ++;
+            }
+        }
+    }
+
+    if (iguais == 0) {
+        printf("Não há elementos em comum\n");
+    }
 }

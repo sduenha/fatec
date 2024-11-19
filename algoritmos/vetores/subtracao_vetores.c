4449f1758a4b76d1611480b53976b7c99faa45e1
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-19 13:18:57 +0000

    Nov 19, 2024, 1:18 PM

--- algoritmos/vetores/subtracao_vetores.c
+++ algoritmos/vetores/subtracao_vetores.c
@@ -5,6 +5,7 @@ int main(void) {
 
     double mat1[MAX][MAX];
     double mat2[MAX][MAX];
+    double mat3[MAX][MAX];
     int i, j, n;
 
     printf("Dimensões das matrizes (max. 10): ");
@@ -17,11 +18,24 @@ int main(void) {
         }
     }
 
+    printf("Lendo os dados da matriz 2, linha por linha\n");
+    for (i = 0; i <  n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%lf", &mat2[i][j]);
+        }
+    }
+
+    for (i = 0; i <  n; i++) {
+        for (j = 0; j < n; j++) {
+            mat3[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
     printf("\n");
 
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
-            printf("%.2lf ", mat1[i][j]);
+            printf("%.2lf ", mat3[i][j]);
         }
         printf("\n");
     }

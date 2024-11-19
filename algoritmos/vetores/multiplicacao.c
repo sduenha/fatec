e0733b77f4e4d642837d3c17e17f000e8cebb82f
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-19 13:36:36 +0000

    Nov 19, 2024, 1:36 PM

--- algoritmos/vetores/multiplicacao.c
+++ algoritmos/vetores/multiplicacao.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#define MAX 10
 
 int main(void) {
     
+    double mat1[MAX][MAX];
+    double mat2[MAX][MAX];
+    double mult[MAX][MAX];
+    int i, j, n;
+
+    printf("Dimensões das matrizes (max. 10): ");
+    scanf("%d", &n);
+
+    printf("Lendo os dados da matriz 1, linha por linha\n");
+    for (i = 0; i <  n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%lf", &mat1[i][j]);
+        }
+    }
+
+    printf("Lendo os dados da matriz 2, linha por linha\n");
+    for (i = 0; i <  n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%lf", &mat2[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i ++) {
+        for (j = 0; j < n; j++) {
+            mult[j][i] = mat1[i][j] * mat2[i][j];
+        }
+    }
 }

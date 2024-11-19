358eb6c64b0cc764d41f5f701967246fac0d3648
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-19 12:57:54 +0000

    Nov 19, 2024, 12:57 PM

--- algoritmos/listas/lista2/exercicio_03.c
+++ algoritmos/listas/lista2/exercicio_03.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int n, valor = 0;
 
     printf("Escolha um número: ");
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++){
+    for (int i = 1; i <= n; i++)
+    {
         valor += i;
     }
 
---
+++ algoritmos/vetores/subtracao_vetores.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 10;
+    int v1[n];
+    int v2[n];
+    int v3[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        v3[i] = v1[i] - v2[i];
+    }
+}

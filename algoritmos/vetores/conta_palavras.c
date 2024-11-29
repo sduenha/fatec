1ac28edef5b8e519ea9ef1e4186290dab0474e5d
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-29 11:27:10 +0000

    Nov 29, 2024, 11:27 AM

--- algoritmos/vetores/conta_palavras.c
+++ algoritmos/vetores/conta_palavras.c
@@ -5,24 +5,32 @@ int main()
 {
     char st1[80], palavra[40], escolha[40];
     int i = 0, j = 0, palavras = 0, palavras_iguais = 0;
+
     printf("Escreve uma frase de no máximo 79:" );
     fgets(st1, 80, stdin);
+
     printf("Escolha uma palavra: ");
     scanf("%s", escolha);
 
     while (st1[i] != '\n' && st1[i] != '\0') {
-        if (st1[i] == ' ') {
+
+        while (st1[i] == ' ') {
             i++;
         }
+
+        j = 0;
         while (st1 != ' ' && st1 != '\n' && st1 != '\0') {
-            j = 0;
             palavra[j] = st1[i];
             j++;
             i++;
-            palavras++;
         }
-        if (strcmp(escolha, palavra) == 0) {
-            palavras_iguais ++;
+        palavra[j] = '\0';
+
+        if (strlen(palavra) > 0)  {
+            palavras++;
+            if (strcmp(escolha, palavra) == 0) {
+                palavras_iguais ++;
+            }
         }
     }
 

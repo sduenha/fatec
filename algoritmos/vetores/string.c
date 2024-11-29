59eaf924093eb662c96d5eb413f70ba017aa747e
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-11-29 11:58:32 +0000

    Nov 29, 2024, 11:58 AM

--- algoritmos/vetores/string.c
+++ algoritmos/vetores/string.c
@@ -2,7 +2,7 @@
 
 int main(void) {
     char string[80], reversa[80];
-    int length = 0, counter = 0;
+    int length = 0, counter = 0, j = 0;
 
     printf("Digite 79 caracteres: ");
     scanf("%s", string);
@@ -11,9 +11,14 @@ int main(void) {
         length ++;
     }
 
+    for (int i = 0, j = length; i < length; i++, j--) {
+        reversa[i] = string[j];
+
+    } 
     while (counter != length - 1) {
         reversa[counter] = string[length];
         counter ++;
+
     }
 
     printf("\n");

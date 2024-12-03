90aeec8c349ac67639fe054752c89b32c5b0d705
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-12-03 21:51:31 +0000

    Dec 3, 2024, 9:51 PM

--- algoritmos/desafios/desafio_03/desafio3.c
+++ algoritmos/desafios/desafio_03/desafio3.c
@@ -3,15 +3,19 @@
 #include <ctype.h>
 
 int main(void) {
-    char senha[50], inversa[50];
+    char senha[50], inversa[50], dicionario[50][20];
     int num_dicionario;
 
     printf("Quantas palavra terá o dicionário? ");
     scanf("%d", &num_dicionario);
 
-    char dicionario[num_dicionario];
     printf("Digite as palavras, uma por linha: \n");
     for (int i = 0; i < num_dicionario; i ++) {
         scanf("%s", dicionario[i]);
     }
+
+    printf("Digite a sua senha: ");
+    scanf("%s", senha);
+
+    
 }

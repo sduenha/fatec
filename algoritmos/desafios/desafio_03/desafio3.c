c47e2e2d32af0f60c960ad52678dbac754fd11f5
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-12-03 22:05:53 +0000

    Dec 3, 2024, 10:05 PM

--- algoritmos/desafios/desafio_03/desafio3.c
+++ algoritmos/desafios/desafio_03/desafio3.c
@@ -4,7 +4,7 @@
 
 int main(void) {
     char senha[50], inversa[50], dicionario[50][20], erros[7][50];
-    int num_dicionario, tamanho_senha, count_erros = 0;
+    int num_dicionario, tamanho_senha, count_erros = 0, verificacao[7];
 
     printf("Quantas palavra terá o dicionário? ");
     scanf("%d", &num_dicionario);
@@ -20,9 +20,20 @@ int main(void) {
     tamanho_senha = strlen(senha);
 
     if (tamanho_senha < 8) {
+        verificacao[0] = 1;
         strcpy(erros[count_erros], "A senha deve conter pelo menos 8 caracteres");
         count_erros ++;
     }
 
-    
+    for (int i = 0; i < tamanho_senha; i ++) {
+        if (islower(senha[i])) {
+            verificacao[1] = 1;
+        } else if (isupper(senha[i])) {
+            verificacao[2] = 1;
+        } else if (isdigit(senha[i])) {
+            verificacao[3] = 1;
+        } else if(senha[i] == "!" || senha[i] == "?" || senha[i] == "#" || senha[i] == "@" || senha[i] == "$") {
+            verificacao[4] = 1;
+        }
+    }
 }

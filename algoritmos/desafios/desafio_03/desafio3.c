ce43f79c469b804ae1da2bec096bd617059bb591
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-12-05 21:51:23 +0000

    Dec 5, 2024, 9:51 PM

--- algoritmos/desafios/desafio_03/desafio3.c
+++ algoritmos/desafios/desafio_03/desafio3.c
@@ -4,20 +4,25 @@
 #include <ctype.h>
 
 int main(void) {
+    // Declaração de variáveis
     char senha[50], inversa[50], dicionario[50][20], erros[7][50];
     int num_dicionario, tamanho_senha, count_erros = 0, verificacao[4] = {0, 0, 0, 0}, j;
 
+    // Pedindo ao usuário o número de palavras do dicionário
     printf("Quantas palavra terá o dicionário? ");
     scanf("%d", &num_dicionario);
 
+    // Adicionando as palavras ao dicionário
     printf("Digite as palavras, uma por linha: \n");
     for (int i = 0; i < num_dicionario; i ++) {
         scanf("%s", dicionario[i]);
     }
 
+    // Pedindo a senha do usuário
     printf("Digite a sua senha: ");
     scanf("%s", senha);
 
+    // Descobrindo o tamanho da senha
     tamanho_senha = strlen(senha);
 
     if (tamanho_senha < 8) {

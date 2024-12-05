0fba6f3a7a2e469bf6a2478924522037bbe1eb37
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2024-12-05 21:29:14 +0000

    Dec 5, 2024, 9:29 PM

--- algoritmos/desafios/desafio_03/desafio3.c
+++ algoritmos/desafios/desafio_03/desafio3.c
@@ -61,7 +61,6 @@ int main(void) {
     }
 
     for (int i = 0; i < num_dicionario; i ++) {
-        printf("%s -> %s\n", senha, dicionario[i]);
         if (strstr(senha, dicionario[i])) {
             printf("A senha nao pode conter palavras reservadas\n");
             count_erros ++;

1e88480ff3a2537960c261c0063b17ec8ca4898a
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-02-12 13:15:25 +0000

    Mensagem explicativa sobre o que você fez

---
+++ algoritmos/areaRetangulo/areaRetangulo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int altura, largura;
+
+    printf("Escreva a altura e a largura de um retângulo: ");
+    scanf("%d %d", &altura, &largura);
+
+    printf("A área do triângulo é: %d\n", (altura * largura));
+}

a463eb5fbb5a691d26cff28bfdc7d589db71b8c7
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-02-21 15:06:12 +0000

    modified:   algoritmos/areaRetangulo/areaRetangulo 	modified:   algoritmos/areaRetangulo/areaRetangulo.c

--- algoritmos/areaRetangulo/areaRetangulo.c
+++ algoritmos/areaRetangulo/areaRetangulo.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+int pedeNumero(char *pedido);
+int area_retangulo(int b, int h);
+
 int main(void) {
 
-    int altura, largura;
+    int altura = pedeNumero("Qual é a altura: ");
+    int largura = pedeNumero("Qual é a largura: ");
 
-    printf("Escreva a altura e a largura de um retângulo: ");
-    scanf("%d %d", &altura, &largura);
+    printf("A área do retângulo é: %d\n", area_retangulo(altura, largura));
+}
 
-    printf("A área do triângulo é: %d\n", (altura * largura));
+int pedeNumero(char pedido[]) {
+    int num;
+    printf("%s", pedido);
+    scanf("%d", &num);
+    return num;
+}
+
+int area_retangulo(int b, int h) {
+    return (b * h);
 }

454cbbf168090aa98378997a87b0071f47b65935
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-04-22 15:22:15 +0000

    new file:   algoritmos/lista6/ex1.c

---
+++ algoritmos/lista6/ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct {
+    char nome[80];
+    double preco;
+    int quantidade;
+} Produto;
+
+void ordenaPreco(Produto vet[], int n) {
+    for (int i = 0; i < n; i++);
+}
+
+void ordenaQuant(Produto vet[], int n) {
+
+}
+
+int main() {
+
+}

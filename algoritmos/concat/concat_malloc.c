9813e08858a193f3e204b0f1c03e69bb985abf49
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-04-25 14:59:19 +0000

    modified:   algoritmos/concat/concat_malloc.c

--- algoritmos/concat/concat_malloc.c
+++ algoritmos/concat/concat_malloc.c
@@ -4,38 +4,44 @@
 
 char *concatena(char *s1, char *s2) {
     int contador = 0;
+    char *sres = NULL;
+    int t1, t2;
+    
+    t1 = strlen(s1);
+    t2 = strlen(s2);
 
-    char *s = malloc(sizeof(s1) + sizeof(s2));
+    sres = malloc((t1 + t2 + 1) * sizeof(char));
 
-    for (int i = 0; i < strlen(s1); i++, contador++) {
-        s[contador] = s1[i];
+    for (int i = 0; i < t1; i++, contador++) {
+        sres[contador] = s1[i];
     }
 
-    for (int i = 0; i < strlen(s2); i++, contador++) {
-        s[contador] = s2[i];
+    for (int i = 0; i < t2; i++, contador++) {
+        sres[contador] = s2[i];
     }
 
-    s[contador] = '\0';
+    sres[contador] = '\0';
 
-    return s;
+    return sres;
 }
 
 int main(void) {
     char s1[100], s2[100];
+    char *pointer = NULL;
 
     printf("Digite a primeira string: ");
     fgets(s1, 100, stdin);
-    s1[strlen(s1)-1] = '\0';
+    s1[strlen(s1) - 1] = '\0';
 
     printf("Digite a segunda string: ");
     fgets(s2, 100, stdin);
-    s2[strlen(s2)-1] = '\0';
+    s2[strlen(s2) - 1] = '\0';
 
-    char *pointer = concatena(s1, s2);
+    pointer = concatena(s1, s2);
 
-    printf("%s", pointer);
-
-    printf("\n");
+    printf("%s \n", pointer);
+    
+    free(pointer);
 
     return 0;
 }

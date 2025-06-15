c5b107ebeab0323447492057c4144706422334d0
Samuel Duenha <72750401+sduenha@users.noreply.github.com>
2025-06-15 12:12:43 +0000

    modified:   algoritmos/laboratorio-arq/aux/lab-imagem.c

--- algoritmos/laboratorio-arq/aux/lab-imagem.c
+++ algoritmos/laboratorio-arq/aux/lab-imagem.c
@@ -1,3 +1,5 @@
+// Grupo: Giullia Ladislau - 2760482421044, Matheus Santana - 2760482421014, Samuel Duenha - 2760482421016, Yan Mazzucatto - 2760482421017
+// Descrição do programa: O programa visa colocar filtros em imagens, ele recebe como parâmetros o nome do arquivo de entrada e o nome do arquivo de saida, depois pede o nome do filtro que vai utilizar (atualmente só tem dois filtros implementados, "cinza" e "esticar"), depois o programa roda e devolve no arquivo de saida a imagem com o filtro aplicado
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +7,8 @@
 
 #define MAX 129
 
+// Faz a leitura de um arquivo de imagem
+// ERRO: Avisa quando não consegue abrir a imagem
 void readImage(char *name, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int *cols, int *rows){
 	int i, j, lixo;
 	char p;
@@ -27,6 +31,7 @@ void readImage(char *name, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], in
 	fclose(f);
 }
 
+// Faz a escrita de um arquivo de imagem
 void writeImage(char *arqSaida, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int cols, int rows){
   	int i, j;
   	FILE *f;
@@ -46,6 +51,7 @@ void writeImage(char *arqSaida, int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX
 	fclose(f);
 }
 
+// Aplica o filtro de esticar, pega todos os valores de cor da imagem encontra o maior e o menor e faz um conta para pegar o novo valor de RGB
 void esticarContraste(int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int novaR[MAX][MAX], int novaG[MAX][MAX], int novaB[MAX][MAX], int cols, int rows){
 	int i, j, minR = R[0][0], maxR = 0, minG = G[0][0], maxG = 0, minB = B[0][0], maxB = 0;
 	for (i = 0; i < rows; i++) {
@@ -68,6 +74,7 @@ void esticarContraste(int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int nov
 	}
 } 
 
+// Aplica o filtro cinza, pega todos os valores de cor do pixel, faz um média e aplica no RGB o mesmo valor
 void escalaCinza(int R[MAX][MAX], int G[MAX][MAX], int B[MAX][MAX], int novaR[MAX][MAX], int novaG[MAX][MAX], int novaB[MAX][MAX], int cols, int rows){
 	int i, j;
 	for (i = 0; i < rows; i++) {
@@ -95,22 +102,23 @@ int main(int argc, char **argv) {
   int R[MAX][MAX], G[MAX][MAX], B[MAX][MAX], novaR[MAX][MAX], novaG[MAX][MAX], novaB[MAX][MAX];
   int cols, rows;
 
+  // Pega o filtro selecionado
   scanf("%s", efeito);
 
+  // Faz a leitura do arquivo desejado
   readImage(arqEntrada, R, G, B, &cols, &rows);
 
-  if (strcmp(efeito, "cinza") == 0){
+  if (strcmp(efeito, "cinza") == 0){ // Vê se o filtro é o cinza, se for aplica o filtro na imagem
 	escalaCinza(R, G, B, novaR, novaG, novaB, cols, rows);
-  } else if (strcmp(efeito, "esticar") == 0) {
+  } else if (strcmp(efeito, "esticar") == 0) { // Vê se o filtro é o estica, se for aplica o filtro na imagem
 	esticarContraste(R, G, B, novaR, novaG, novaB, cols, rows);
-  } else {
+  } else { // Se o filtro selecionado não estiver feito ele retorna com aviso para o usuário
 	printf("O efeito escolhido não existe!\n");
 	return 1;
   }
 
+  // Depois que passou pelo filtro ele escreve a nova imagem no arquivo de saida
   writeImage(arqSaida, novaR, novaG, novaB, cols, rows);
-  
-
 
   return 0;
 }
